208f033343ae482a6cfac51346ebb317514220d7
brn2bwild <brn_2_b_wild@hotmail.com>
2023-05-18 23:24:12 -0600

    Agregada la ruta y vista de la encuesta

---
+++ resources/js/Pages/Survey.vue
@@ -0,0 +1,15 @@
+<template>
+	<Head title="Encuesta" />
+
+	<div>
+
+	</div>
+</template>
+
+<script setup>
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<style lang="scss" scoped>
+
+</style>
